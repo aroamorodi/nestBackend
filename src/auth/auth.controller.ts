e74872a0f368ff6564b4319de731f8cff9618ae7
aroamorodi <aroa.moro-diaz@capgemini.com>
2024-06-13 12:36:15 +0200

    Validar JWT y obtener usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
@@ -27,7 +27,7 @@ export class AuthController {
 
   @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@Request() request: Request) {
     return this.authService.findAll();
   }
 
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,18 +1,41 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
+import { JwtPayload } from '../interfaces/jwt-payload';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor (
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private authService: AuthService
   ) {}
 
-  canActivate(context: ExecutionContext,): Promise<boolean> {
+  async canActivate(context: ExecutionContext,): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    return Promise.resolve(true);
+    if (!token) {
+      throw new UnauthorizedException('No hay token en la petición');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(
+        token,
+        {
+          secret: process.env.JWT_SEED
+        }
+      );
+
+      const user = await this.authService.findUserById(payload.id);
+      if (!user) throw new UnauthorizedException('User does not exists');
+      if (!user.isActive) throw new UnauthorizedException('User is not active');
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      request['user'] = user;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
   }
 
 
