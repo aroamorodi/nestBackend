05abcb28c63202873d60baa339fa84cebf3a9047
aroamorodi <aroa.moro-diaz@capgemini.com>
2024-06-13 09:36:29 +0200

    Cambios en el env.template para la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    AuthModule, 
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot()
+  ],
   controllers: [],
   providers: [],
 })
