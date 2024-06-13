22a60c4579f17ab390d9ebc3df37bb03cd8e4d87
aroamorodi <aroa.moro-diaz@capgemini.com>
2024-06-13 09:57:12 +0200

    Añadidas anotaciones de Prop en mi clase de User y exportarla como SchemaFactory

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,11 +1,18 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 @Schema()
 export class User {
     //_id: string; LO VA A COLOCAR MONGO POR MI
+    @Prop( {unique: true, required: true})
     email: string;
+    @Prop({required: true})
     name: string;
+    @Prop({minlength: 6, required: true})
     password: string;
+    @Prop({default: true})
     isActive: boolean;
-    roles: string;
+    @Prop({type: [String], default: ['user']}) //['user', 'admin']
+    roles: string[];
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
